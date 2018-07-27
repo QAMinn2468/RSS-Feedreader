461b7e4fea99234a6cae57730b74ff2bbdc274bd
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-07-27 19:23:36 -0400

    Suite 1 passes review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -10,54 +10,50 @@ $(function() {
 
         it('URL is defined', function() {                                       // correct.
           for(i=0; i<allFeeds.length; i++) {
+            expect(allFeeds[i].url).toBeDefined();                              // mirror style of initial test.
             expect(allFeeds[i].url).not.toBe('');                               // correct! - loop through allFeeds array, verify url: is not an empty string.
         };
         });
 
         it('name is defined', function() {                                      // correct.
         for(i=0; i<allFeeds.length; i++) {
-            expect(allFeeds[i].name).not.toBe('');                              // correct! - loop through allFeeds array, verify name: is not an empty string
+          expect(allFeeds[i].name).toBeDefined();                               // mirror style of the initial test.
+          expect(allFeeds[i].name).not.toBe('');                                // correct! - loop through allFeeds array, verify name: is not an empty string
         };
         });
     });
-}());                                                                           //  Suite 1 complete!
+}());                                                                           //  Suite 1 complete and passed Review!!
 
+//
+// $(function() {
+//     describe('The menu', function() {                                           // correct.
+//
+//       it('is hidden', function() {                                              // correct.
+//
+//         expect($('body').hasClass('menu-hidden')).toBe(true);                   // Works!
+//       });
+//
+//       it('changes when clicked', function() {                                   // correct.
+//
+//         expect(document.body.getElementsByClassName('menu-hidden')).not.toBe('');  // use .hasClass('menu-hidden') method.
+//         expect(document.body.getElementsByClassName('menu-hidden')).toBe('');   // TODO: needs to listen for click event / double click event.
+//       });
+//     });
+//
+//
+//          /* TODO: Write a test that ensures the menu changes
+//           * visibility when the menu icon is clicked. This test
+//           * should have two expectations: does the menu display when
+//           * clicked and does it hide when clicked again.
+//           */
+// }());
 
-$(function() {
-    describe('The menu', function() {                                           // correct.
-
-      it('is hidden', function() {                                              // correct.
-
-        expect(document.body.getElementsByClassName('menu-hidden')). not.toBe('');  //WORKS!
-      });
-
-      it('changes when clicked', function() {                                   // correct.
-
-        expect(document.body.getElementsByClassName('menu-hidden')).not.toBe('');  //"hidden is not defined"
-        expect(document.body.getElementsByClassName('menu-hidden')).toBe('');
-      });
-    });
-
-        /* TODO: Write a test that e
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-}());
-
-
+// 
 // $(function() {
 //   describe('Initial Entries', function() {                                      // correct.
 //
 //     it('status of .feed', function() {                                          // correct.
-//
-//       expect(feed.classlist.contains('entry')).toBe(true);                      // Empty function Passes. - verify .feed containter contains the class entry.
+//       expect($('.feed.entry').length).not.toBe(0);
 //
 //     });
 //   });
@@ -72,22 +68,22 @@ $(function() {
 
 
 
-$(function() {
-  describe('New Feed Selection', function() {                                   // correct.
-
-    it('verify content is new', function() {                                    // correct.
-
-      expect(feedList).not.toBe(feedNew);                                       // Passes! New feed does not match old feed.
-    });
-
-    // it('verify each entry is unique', function() {                              // correct
-    //
-    //   expect(entry).not.equals(entry);
-    // })
-  });
-
-
-          // /* TODO:  verify each .entry-link is unique. - MINE
-          // */
-
-}());
+// $(function() {
+//   describe('New Feed Selection', function() {                                   // correct.
+//
+//     it('verify content is new', function() {                                    // correct.
+//
+//       expect()                                    // Passes! New feed does not match old feed.
+//     });
+//
+//     it('verify each entry is unique', function() {                              // correct
+//
+//       expect()
+//     })
+//   });
+//
+//
+//           // /* TODO:  verify each .entry-link is unique. - MINE
+//           // */
+//
+// }());
