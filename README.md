7caec37d4d98a43de5af446db40e7baa3b236417
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-08-03 10:40:15 -0400

    Updated README

--- README.md
+++ README.md
@@ -2,31 +2,41 @@
 # Welcome!
 
 
-In order to run this application, you will need to go to https://qaminn2468.github.io/frontend-nanodegree-feedreader/ .  Open the hamburger icon to view the different RSS feeds.  Click on one to choose it.  Then review the list of entries, clicking on one that interests you.
+In order to run this application, you will need to go to https://qaminn2468.github.io/frontend-nanodegree-feedreader/ .  Click 'download'.  Click 'download zip'.  Choose where you would like the file to reside. (In this example, we will use the Chrome browser.) Open Chrome.  Press CTRL-O.  Choose index.html from within the downloaded folder.  The page will load.  It will display articles from the Navy Times.  To choose other feed, open the hamburger icon.  
 
 
 
 
 ## Choices
 
-The choices of feed are:
-
--Navy Times
--Veterans
--Military Family
--Transition
+The choices of article topics are:
+<ul>
+<li>Navy Times</li>
+<li>Veterans</li>
+<li>Military Family</li>
+<li>Transition</li></ul>
 
 
 
 
 ### Options
 
--Right click to choose to open the entry in a new window.
+-Right click to open the article into a new window.  This allows the feed to remain accessible.
 
 
 
+### Ideas for Others
+
+-Change the RSS feeds to those you would enjoy.
 
 
-### Info
 
-This is the fourth project for the Grow with Google Scholarship of the Udacity FEND nanodegree.  Developed by Kim McCaffrey 2018
+### Footnotes
+
+This is the fourth project for the Grow with Google Scholarship of the Udacity Front-End Development Nanodegree.  
+
+All code is my own.  In my research (because not all tools needed are provided by Udacity), I reviewed documentation by Jasmine, MND, W3schools, and Matthew Cranford. I have deleted the commit I made after reading Matthew's work. I will consider walkthroughs and articles about projects I have not completed to be tainted and to be studiously avoided. I feel betrayed that those same walkthroughs are brilliant and inspired and display a real gift for teaching, but to partake of those gifts and learn from them, results in discipline and threat of expulsion.
+
+Enjoy!
+KMcC
+August 2018
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -30,13 +30,13 @@ $(function() {
 
       it('is hidden', function() {                                              // correct.
 
-        expect($('body').hasClass('menu-hidden')).toBe(true);                   // Works!
+        expect($('body').hasClass('menu-hidden')).toBe(true);                   // when page loads, menu is hidden.
       });
 
       it('menu icon changes menu-hidden', function() {                          // correct.
-        menu.click();                                                           // click - class Menu-hidden is removed.
+        menu.click();                                                           // click - class Menu-hidden is removed. Menu is shown.
         expect($('body').hasClass('menu-hidden')).toBe(false);
-        menu.click();                                                           // second click - class Menu-hidden is returned.
+        menu.click();                                                           // second click - class Menu-hidden is returned. Menu hides once more.
         expect($('body').hasClass('menu-hidden')).toBe(true);
       });
     });
@@ -46,12 +46,12 @@ $(function() {
 $(function() {
   describe('Initial Entries', function() {                                      // correct.
 
-    beforeEach(function loadFeed(done) {
+    beforeEach(function loadFeed(done) {                                        // Before running test, ensure loadFeed() is complete.
       done();
     });
 
     it('status of feed', function() {                                           // correct.
-      expect($('feed.entry')).not.toBe(0);
+      expect($('feed.entry')).not.toBe(0);                                      // array of feed entries should not be empty.
     });
   });
 }());                                                                           // Suite 3 complete!
@@ -60,15 +60,25 @@ $(function() {
 
 $(function() {
   describe('New Feed Selection', function() {                                   // correct.
+    let load1 ;                                                                 // declare variable load1
+    beforeEach(function loadFeed(done) {                                        // fill variable with content.
+      done();
+      load1 = this;
+    });
+
+    let load2;                                                                  // declare variable
+    beforeEach(function loadFeed(done) {
+      done();                                                                   // fill variable with content.
+      load2 = this;
+    });
 
     it('verify content is new', function() {                                    // correct.
-
-      expect()
+      expect(load1 === load2).toBe(false);                                      // verify old feed is not equal to new feed.
     });
 
     it('verify each entry is unique', function() {                              // correct
-
-      expect()
-    })
+      for (i = 0 , i < feed.length, i++) {                                      // iterate through the array, verify all entries are unique.
+      expect(feed.entry[i] !=== feed.entry[i+1]).toBe(true);
+    }})
   });
 }());
