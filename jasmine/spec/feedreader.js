fdba75d50be4fa0f0b4c9690f75fb8010ada1909
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-08-11 12:57:43 -0400

    Update 12:58

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,37 +54,37 @@ $(function() {
       expect($('feed.entry')).not.toBe(0);                                      // array of feed entries should not be empty.
     });
   });
-                                                                        // Suite 3 complete!
+  }());                                                                       // Suite 3 complete!
 
 
 
-$(function() {
-  const loadA = [];
-  const loadB = [];
-  const fE = feed.entry;
-  describe('New Feed Selection', () =>
-
-
-    beforeEach(done =>
-
-      loadFeed(2);
-      Array from(fE).forEach(entry =>
-      loadA.push(entry.innerText);
-      console.log('Suite 4 before Each'));
-
-      loadFeed(3, done);
-      Array from(fE).forEach(entry =>
-      loadB.push(entry.innerText));
-      );
-
-    it('verify content is new', done =>
-      expect(loadA === loadB).toBe(false);                                      // verify old feed is not equal to new feed.
-      done();                                                                   // inspired by chapter 22-18 'Async Address book'
-    );
-
-    // it('verify each entry is unique', function() {             // TODO:                  // correct  remove "x" when ready to run.
-    //   for (i = 0 , i < feed.length, i++) {                                      // iterate through the array, verify all entries are unique.
-    //   expect(feed.id[i] !=== feed.id[i+1]).toBe(true);
-    //
-
-}());
+// $(function() {
+//   const loadA = [];
+//   const loadB = [];
+//   const fE = feed.entry;
+//   describe('New Feed Selection', () =>
+//
+//
+//     beforeEach(done =>
+//
+//       loadFeed(2);
+//       Array from(fE).forEach(entry =>
+//       loadA.push(entry.innerText);
+//       console.log('Suite 4 before Each'));
+//
+//       loadFeed(3, done);
+//       Array from(fE).forEach(entry =>
+//       loadB.push(entry.innerText));
+//       );
+//
+//     it('verify content is new', done =>
+//       expect(loadA === loadB).toBe(false);                                      // verify old feed is not equal to new feed.
+//       done();                                                                   // inspired by chapter 22-18 'Async Address book'
+//     );
+//
+//     // it('verify each entry is unique', function() {             // TODO:                  // correct  remove "x" when ready to run.
+//     //   for (i = 0 , i < feed.length, i++) {                                      // iterate through the array, verify all entries are unique.
+//     //   expect(feed.id[i] !=== feed.id[i+1]).toBe(true);
+//     //
+//
+// }());
